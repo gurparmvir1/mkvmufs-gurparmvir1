8dc048ba398746c88ae8d988a0728dc269bed4c2
gurparmvir1 <43353418+gurparmvir1@users.noreply.github.com>
2018-11-06 10:51:38 -0500

    Add files via upload

--- mkvmufs.c
+++ mkvmufs.c
@@ -14,10 +14,13 @@ int main(void)
     int X = (254 * 512);
     unsigned short y = 0xfffc;
     unsigned short z = 0xfffa;
+    unsigned short x = 0x55;
     int a = 242;
+    unsigned short b = 1;
+     unsigned short c = 0;
   //  int z = (241 * 512)-1;
-    int c = (242 * 512)-1;
-    int d = (253 *  512)-1;
+  //  int c = (242 * 512)-1;
+   // int d = (253 *  512)-1;
   
         FILE *fp = fopen("file.img", "wb");
         fseek(fp, X , SEEK_SET);
@@ -32,6 +35,25 @@ int main(void)
         putc(a-1, fp);
         a++;
         //fwrite(&a, sizeof(int), 1, fp);
+    }
+    fwrite(&z, sizeof(z), 1, fp);
+    fwrite(&z, sizeof(z), 1, fp);
+    for(int i=254;i<270;i++)
+    {
+          fwrite(&x, sizeof(x), 1, fp);
+    }
+    // color values and code components sets to 1
+    fwrite(&b, sizeof(b), 1, fp);
+    fwrite(&b, sizeof(b), 1, fp);
+    fwrite(&b, sizeof(b), 1, fp);
+    fwrite(&b, sizeof(b), 1, fp);
+    fwrite(&b, sizeof(b), 1, fp);
+    //putc(1, fp);
+   // putc(1, fp);
+    
+    for(int i=276;i<303;i++)
+    {
+        fwrite(&c, sizeof(c), 1, fp);
     }
     
         fclose(fp);
