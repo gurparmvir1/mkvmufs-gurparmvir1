0212a1d1338df316f7fea72f454c8ef041149398
gurparmvir1 <43353418+gurparmvir1@users.noreply.github.com>
2018-11-05 21:02:52 -0500

    Add files via upload

--- mkvmufs.c
+++ mkvmufs.c
@@ -13,18 +13,27 @@ int main(void)
   //  int f = 4;
     int X = (254 * 512);
     unsigned short y = 0xfffc;
-    int z = (241 * 512)-1;
+    unsigned short z = 0xfffa;
+    int a = 242;
+  //  int z = (241 * 512)-1;
     int c = (242 * 512)-1;
     int d = (253 *  512)-1;
   
         FILE *fp = fopen("file.img", "wb");
         fseek(fp, X , SEEK_SET);
-   // for(int i=0;i<X;i++)
-   // {
+    for(int i=0;i<240;i++)
+    {
         fwrite(&y, sizeof(y), 1, fp);
-            // fputs("0xFFFC", fp);
     
-   
+    }
+    fwrite(&z, sizeof(z), 1, fp);
+    for(int i=241;i<253;i++)
+    {
+        putc(a-1, fp);
+        a++;
+        //fwrite(&a, sizeof(int), 1, fp);
+    }
+    
         fclose(fp);
     return 0;
 
