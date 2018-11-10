27fb9a86c3e9aecc1cd8913abf8f3d214f2eb0f5
gurparmvir1 <43353418+gurparmvir1@users.noreply.github.com>
2018-11-10 10:58:13 -0500

    Add files via upload
    Final part1

--- mkvmufs.c
+++ mkvmufs.c
@@ -8,8 +8,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#include<math.h>
+
 int main(void)
 {
+    //local time for timestamps
+    time_t timer;
+    struct tm* info;
+    char day[2];
+    char month[2];
+    char year[4];
+    char hour[2];
+    char min[2];
+    char sec [2];
+    char wday[2];
+    time(&timer);
+    info = localtime(&timer);
+    strftime(day, 2, "%d", info);
+    strftime(month, 2, "%m", info);
+    strftime(year, 4, "%Y", info);
+    strftime(hour, 2, "%H", info);
+    strftime(min, 2, "%M", info);
+    strftime(sec, 2, "%S", info);
+    strftime(wday, 1, "%w", info);
+    
+    
+    // intializing number of bytes
     int a = 242;
     int X = (254 * 512);
     unsigned short y = 0xfffc;
@@ -23,10 +49,11 @@ int main(void)
     unsigned short g = 0x000D;
     unsigned short h = 1;
     unsigned short i = 0x00C8;
-  
-  
-        FILE *fp = fopen("file.img", "wb");
-        fseek(fp, X , SEEK_SET);
+    
+    
+
+    FILE *fp = fopen("file.img", "wb");
+    fseek(fp, X , SEEK_SET);
     for(int i=0;i<240;i++)
     {
         fwrite(&y, sizeof(y), 1, fp);
@@ -37,10 +64,12 @@ int main(void)
     {
         putc(a-1, fp);
         a++;
-        //fwrite(&a, sizeof(int), 1, fp);
     }
+    //writing 0xFFFA
     fwrite(&z, sizeof(z), 1, fp);
     fwrite(&z, sizeof(z), 1, fp);
+    
+    //writing 0x55
     for(int i=254;i<270;i++)
     {
           fwrite(&x, sizeof(x), 1, fp);
@@ -51,46 +80,59 @@ int main(void)
     fwrite(&b, sizeof(b), 1, fp);
     fwrite(&b, sizeof(b), 1, fp);
     fwrite(&b, sizeof(b), 1, fp);
-    //putc(1, fp);
-   // putc(1, fp);
     
+    //set 27 bytes to 0
     for(int i=276;i<303;i++)
     {
         fwrite(&c, sizeof(c), 1, fp);
     }
     
-    // filesystem creation
+    // timestamps
     
+    fwrite(&year, sizeof(year), 1, fp);
+    fwrite(&month, sizeof(month), 1, fp);
+    fwrite(&day, sizeof(day), 1, fp);
+    fwrite(&hour, sizeof(hour), 1, fp);
+    fwrite(&min, sizeof(min), 1, fp);
+    fwrite(&sec, sizeof(sec), 1, fp);
+    fwrite(&wday, sizeof(wday), 1, fp);
     
+    //set 40 bytes to 0
     
     for( int i=312;i<326;i++)
     {
         fwrite(&c, sizeof(c), 1, fp);
     }
+    //writing 0x00FE
     fwrite(&d, sizeof(d), 1, fp);
     fwrite(&d, sizeof(d), 1, fp);
     
+    //writing 0x0001
     fwrite(&e, sizeof(e), 1, fp);
     fwrite(&e, sizeof(e), 1, fp);
     
+    //writing 0x00FD
     fwrite(&f, sizeof(f), 1, fp);
     fwrite(&f, sizeof(f), 1, fp);
     
+    //wrinting 0x000D
     fwrite(&g, sizeof(g), 1, fp);
     fwrite(&g, sizeof(g), 1, fp);
     
+    ////wrting 0-230 random
     fwrite(&h, sizeof(h), 1, fp);
     fwrite(&h, sizeof(h), 1, fp);
    
+    //writing 0x00C8
     fwrite(&i, sizeof(i), 1, fp);
     fwrite(&i, sizeof(i), 1, fp);
     
-
+    //set all bytes 430  to 0
     for( int i=338;i<768;i++)
     {
         fwrite(&c, sizeof(c), 1, fp);
     }
-
+    
         fclose(fp);
     return 0;
 
